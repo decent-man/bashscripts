1a65519c2ebb9c4ef31070ad01c7454a6d175105
whotftookmyname <toomachpiece@gmail.com>
2021-02-06 18:24:57 +0530

    Improved README and cleaned up Script

--- dsearch/README.md
+++ dsearch/README.md
@@ -1,6 +1,6 @@
 # DSEARCH
 ## Search quicker with dmenu
-    A script to lookup stuff from your favorite websites
+A script to lookup stuff from your favorite websites
 
 ### Usage
 
@@ -14,10 +14,11 @@ The key here is customizing the script to your liking and needs.
 You can use the script without modifying it at all, just that these are my defaults and not tailored specifically for you.
 
 Usage totally depends on how you customized it.
-Note that I have specifically tailored it for using it with my qutebrowser setup, so it loads up with all of my qutebrowser quickmarks by default.
-But you can use it without it as well. Just that you won't have the quickmarks listed as I do. If you can get your browser's bookmarks conveniently then be sure to check out "####"
 
-Be sure to change browser(__$BRW__) in the configuration if you use/prefer a different browser.
+Note that I have specifically tailored it for using it with my qutebrowser setup, so it loads up with all of my qutebrowser quickmarks by default.
+But you can use it without it as well. Just that you won't have the quickmarks listed as I do. If you can get your browser's bookmarks conveniently then be sure to check out [Custom Quickmark](https://github.com/whotftookmyname/bashscripts/blob/main/dsearch/README.md#configuration)
+
+Be sure to change browser(__$BRW__) in the configuration if you use/prefer a different browser.[Changing Browser]()
 
 Upon being called shows up with a list of quickmarks(Only if you use qutebrowser and have quickmarks):
 
@@ -26,6 +27,8 @@ Upon being called shows up with a list of quickmarks(Only if you use qutebrowser
 However these are the **defaults**:
 #### Commands
 To be typed inside dmenu
+- **ArchWiki**
+    - `aw search-text` : Searches Archwiki for the specified query. E.g to lookup xmobar on archwiki type `aw xmobar` and Enter.
 - **Youtube**
     - `yt hm` : Opens up your YouTube homepage.
     - `yt wl` : Opens up your YouTube watch later page.
@@ -37,11 +40,24 @@ To be typed inside dmenu
     - `ste search-text` : Searches Stack Exchange for your specified query.
 - **SuperUser**
     - `su search-text` : Searches SuperUser for your specified query.
-- **ArchWiki**
-    - `aw search-text` : Searches Archwiki for the specified query. E.g to lookup xmobar on archwiki type `aw xmobar` and Enter.
 - __Wildcard__
+
 By default just typing without any command will search the written text on DuckDuckGo. However there exists a `ddg` command if you ever wish to change your default search engine to something else.
 
 ### Configuration
+
+#### Basic
+
+If you just need to make basic changes then the _BASE VARIABLES_ section is all that you need.
+
+- **Browser**
+    - Change your default browser to be executed by modifying the `BRW` variable. By default it is set to qutebrowser.
+- **Colors & Fonts**
+    - Foreground is set by `dcolor` and Background color is set by `dbcolor`.
+    - Font can be set normally. By default Source Code Pro,size 8 is specified.
+
+#### Advanced
+
+These changes are more suited for a person well familiar with bash syntax.
 
 
--- dsearch/dsearch.sh
+++ dsearch/dsearch.sh
@@ -8,72 +8,82 @@
 #                       ~/.bshScr/dsearch
 #                        Tool to make quick web searches from the desktop
 
-
-#First define functions for YouTube, DuckDuckGo, ArchWiki, Stack Overflow, Stack Exchange, Superuser.
-BRW=qutebrowser
+#=====================================================
+#                 BASE VARIABLES
+#=====================================================
+# The browser
+    BRW=qutebrowser
 # Color for the prompt menu
-#dcolor=#5100FF           #Strong Purple
-#dcolor=#8C00FF           #Light Purple
-#dcolor=#AB960C           #Mango Yellow
-dcolor=#FFFF00           #Yellow
-dbcolor=#282A36           #Solarized~DraculaishGreyishIndigo
-FONT="xft:Source Code Pro:style=Medium:size=8"
+    #Foreground
+      dcolor=#FFFF00            #Yellow
+    #Background
+      dbcolor=#282A36           #DraculaGreyishIndigo
+    #Font
+      FONT="xft:Source Code Pro:style=Medium:size=8"
+#====================================================
+#                   COMMANDS                            -add,remove or modify commands
+#====================================================
+# Launches the links in the specified browser upon being called.
+# Basic layout is:
+#    function shortname(){                                          #the same name is used in the final case statement as well
+#       1.SEARCH_VARIABLE that grabs all words(the '$@') and trims all spaces and converts them into url friendly '+'
+#       2.ANY_SPECIAL_VARIABLES you want to specify for custom use, as done in case of YouTube.
+#       3.EXECUTE: BROWSER LINKASPARAMETER || Write a simple case statement for any custom url manipulation.-As done in case of YouTube[except it is a sophisticated logical case statement :) ]
+#   }
 
-#"DEFAULT": "https://duckduckgo.com/?q={}", "aw": "https://wiki.archlinux.org/index.php?search={}", "yt": " ", "sto": "http://stackoverflow.com/search?q={}", "ste": "http://stackexchange.com/search?q={}", "su": "http://superuser.com/search?q={}"}')
-#https://www.youtube.com/results?search_query=
-function yt(){                                                  # YouTube Search
+#Youtube
+    function yt(){                                                  
         SEARCH=$(echo $@ | tr ' ' '+') 
         WL="https://www.youtube.com/playlist?list=WL"
         HIST="https://www.youtube.com/feed/history"
         YTHOME="https://www.youtube.com/"
- [ "$@" == "wl" ] && $BRW $WL && exit || [ "$@" == "hm" ] && $BRW $YTHOME && exit || [ "$@" == "hi" ] && $BRW $HIST && exit || $BRW https://www.youtube.com/results?search_query=$SEARCH
-#case "$@" in 
-#        wl) $BRW $WL && break;;
-#        hm) $BRW $YTHOME;;
-#        hi) $BRW $HIST;;
-#        *) $BRW https://www.youtube.com/results?search_query=$SEARCH ;;
-#esac
-}
-
-function ddg(){                                                 #DuckDuckGo Search
+        [ "$@" == "wl" ] && $BRW $WL && exit || [ "$@" == "hm" ] && $BRW $YTHOME && exit || [ "$@" == "hi" ] && $BRW $HIST && exit || $BRW https://www.youtube.com/results?search_query=$SEARCH
+    }
+#DuckDuckGo
+    function ddg(){                                                 
         SEARCH=$(echo $@ | tr ' ' '+') 
-$BRW https://duckduckgo.com/?q=$SEARCH
-}
-
-function ste(){                                                 #Stack Exchange Search
+        $BRW https://duckduckgo.com/?q=$SEARCH
+    }
+#Stack Exchange
+    function ste(){                                                
         SEARCH=$(echo $@ | tr ' ' '+') 
-$BRW http://stackexchange.com/search?q=$SEARCH
-}
-
-function sto(){                                                 #Stack Overflow Search
+        $BRW http://stackexchange.com/search?q=$SEARCH
+    }
+#Stack Overflow
+    function sto(){                                               
         SEARCH=$(echo $@ | tr ' ' '+') 
-$BRW http://stackoverflow.com/search?q=$SEARCH
-}
-
-function su(){                                                 #Superuser Search
+        $BRW http://stackoverflow.com/search?q=$SEARCH
+    }
+#SuperUser
+    function su(){                                               
         SEARCH=$(echo $@ | tr ' ' '+') 
-$BRW http://superuser.com/search?q=$SEARCH
-}
-
-function aw(){                                                  #ArchWiki Search
+        $BRW http://superuser.com/search?q=$SEARCH
+    }
+#Arch Wiki
+    function aw(){                                              
         SEARCH=$(echo $@ | tr ' ' '+') 
-$BRW https://wiki.archlinux.org/index.php?search=$SEARCH
-}
+        $BRW https://wiki.archlinux.org/index.php?search=$SEARCH
+    }
 
-#(cat ~/.config/qutebrowser/quickmarks | awk '{print $2}' |dmenu -l 8 | xargs -r qutebrowser) || (echo Hello | dmenu -p "Where to search?:")
-QM=$(cat ~/.config/qutebrowser/quickmarks | awk '{print $2}' |dmenu -l 8 -nb $dbcolor -sf $dbcolor -sb $dcolor -nf $dcolor -fn "$FONT")
+#===================================================================================================================
+#                       MAIN CODE                               -do not mess unless you understand what you're doing
+#===================================================================================================================
+# If on qutebrowser make sure to have all quickmark names as single word names. Else its not going to work. I know I know its just a hack.
+#Pipes my qutebrowser quickmarks to dmenu and also stores the dmenu output
+QM=$(cat ~/.config/qutebrowser/quickmarks | awk '{print $2}' | dmenu -l 8 -nb $dbcolor -sf $dbcolor -sb $dcolor -nf $dcolor -fn "$FONT")
+# Check if output is not null. Most likely the case if your quickmarks are empty and you pressed enter with no args
 [ -z "$QM" ] && exit ;
-#for loop compare && qutebrowser $QM || case 1:yt do $yt $@ ; case 2:ddg do $ddg $@ ; case 3:ste do .....etc.
+# Grab the command from the beginning(if any)
 QMHEAD=$(echo $QM | head -c 3 | tr -d ' ')
-#for((i=0;i<$(cat ~/.config/qutebrowser/quickmarks | wc -l);i=i+1));
+# For loop check if its a 'command' or a 'quickmark' that is to be launched
 for i in $( cat ~/.config/qutebrowser/quickmarks | awk '{print $2}')
-#do [ "$QM" == "${sed -n '{!i}p' ~/.config/qutebrowser/quickmarks | awk '{print $2}')" ] && break ;
 do [ "$QM" == "$i" ] &&  break ;
-done  && qutebrowser $QM || case $QMHEAD in                              yt) yt $(echo $QM | cut -c4- | tr '#' ' ' );;
-                                                                        ddg) ddg $(echo $QM | cut -c5- | tr '#' ' ' );;
-                                                                        ste) ste $(echo $QM | cut -c5-) | tr '#' ' ' ;;
-                                                                        sto) sto $(echo $QM | cut -c5-) | tr '#' ' ' ;;
-                                                                        su) su $(echo $QM | cut -c4- | tr '#' ' ' );;
-                                                                        aw) aw $(echo $QM | cut -c4-| tr '#' ' ' );;
-                                                                         *) ddg $(echo $QM | tr '#' ' ' );;
-                                                                    esac
+done  && qutebrowser $QM || case $QMHEAD in       yt) yt $(echo $QM | cut -c4- | tr '#' ' ' );;         # If a quickmark then launch it with qutebrowser if not run this case statement. CHANGE QUTEBROWSER HERE IF NEEDED. Left unchanged since quickmarks are a qutebrowser thing.
+                                                 ddg) ddg $(echo $QM | cut -c5- | tr '#' ' ' );;
+                                                 ste) ste $(echo $QM | cut -c5-) | tr '#' ' ' ;;
+                                                 sto) sto $(echo $QM | cut -c5-) | tr '#' ' ' ;;
+                                                  su) su $(echo $QM | cut -c4- | tr '#' ' ' );;
+                                                  aw) aw $(echo $QM | cut -c4-| tr '#' ' ' );;
+                                                   *) ddg $(echo $QM | tr '#' ' ' );;
+                       # your_short_command_in_dmenu) shortname $(LINK_FROM_QM | CUT OUT THE COMMAND PART | TRIM any unnecessary characters)
+                                            esac
